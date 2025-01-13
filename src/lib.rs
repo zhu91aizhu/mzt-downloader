c26bec1d8a967ff714f59ae010cad1a8859e1135
mengaily <zhu91aizhu@163.com>
2025-01-13 11:50:54 +0800

    feature: 添加运行命令

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::path::{Path, PathBuf};
 use std::sync::Arc;
 
@@ -7,20 +8,17 @@ use tokio::fs::File;
 use tokio::io::AsyncWriteExt;
 
 async fn get_url_content(client: Arc<Client>, url: &str) -> Result<String> {
-    // let client = client.clone();
-    // let response = client.get(url).send().await?;
-    //
-    // // 没有请求错误
-    // let response = response.error_for_status()?;
-    // let content = response.text().await?;
-    // Ok(content.to_string())
-    Ok("".to_string())
+    let client = client.clone();
+    let response = client.get(url).send().await?;
+    let response = response.error_for_status()?;
+    let content = response.text().await?;
+    Ok(content.to_string())
 }
 
 #[derive(Clone)]
-struct Album {
+pub struct Album {
     client: Arc<Client>,
-    name: String,
+    pub name: String,
     url: String
 }
 
@@ -64,13 +62,13 @@ impl Album {
         Ok(all_pictures)
     }
 
-    async fn download_picture(client: Arc<Client>, url: String, save_to_path: PathBuf) -> Result<()> {
-        let response = client.get(&url).send().await?;
+    async fn download_picture(client: Arc<Client>, url: &str, save_to_path: PathBuf) -> Result<()> {
+        let response = client.get(url).send().await?;
         if !response.status().is_success() {
             return Err(anyhow!("send get picture request error: {}", response.status()))
         }
 
-        let picture_name = Self::get_picture_name(&url)?;
+        let picture_name = Self::get_picture_name(url)?;
         let path = save_to_path.join(picture_name);
         let bytes = response.bytes().await?;
         let mut file = File::create(path).await?;
@@ -90,7 +88,7 @@ impl Album {
             let base_path = path.clone();
             let client = Arc::clone(&self.client);
             let task = tokio::spawn(async move {
-                match Self::download_picture(client, url.clone(), base_path).await {
+                match Self::download_picture(client, &url, base_path).await {
                     Ok(_) => {
                         println!("picture {url} downloaded.");
                     }
@@ -113,32 +111,135 @@ impl Album {
     }
 }
 
-struct AlbumSearcher {
-    client: reqwest::Client,
+pub struct AlbumSearcher {
+    client: Arc<Client>,
     page: u32,
+    page_count: u32,
     size: u32,
     keyword: String,
-    albums: Option<Vec<Album>>
+    albums: HashMap<String, Vec<Album>>
 }
 
 impl AlbumSearcher {
-    fn new(keyword: &str, size: u32) -> Self {
+    pub const DEFAULT_PAGE_SIZE: u32 = 10u32;
+
+    pub fn new(keyword: &str, size: u32) -> Self {
         let mut size = size;
         if size < 1 {
             size = 10;
         }
 
         Self {
-            client: Client::new(),
-            page: 1,
+            client: Arc::new(Client::new()),
+            page: 0,
+            page_count: 0,
             size,
             keyword: keyword.to_string(),
-            albums: None
+            albums: HashMap::new()
         }
     }
 
-    async fn next(&mut self) -> Result<Option<Vec<Album>>> {
-        todo!()
+    fn parse_page_count(&self, html: &str) -> Result<u32> {
+        Ok(1u32)
+    }
+
+    async fn parse_albums(&self) -> Result<(Vec<Album>, u32)> {
+        let url = "";
+
+        let client: Arc<Client> = Arc::clone(&self.client);
+        let html = get_url_content(client, url).await?;
+
+        let page_count = if self.page_count == 0 {
+            self.parse_page_count(&html)?
+        } else {
+            self.page_count
+        };
+
+        // TODO parse albums
+        Ok((vec![], page_count))
+    }
+
+    async fn get_albums(&mut self) -> Result<Option<&Vec<Album>>> {
+        let key = format!("page-{}", &self.page);
+        if self.albums.contains_key(&key) {
+            Ok(self.albums.get(&key))
+        } else {
+            // 获取新数据
+            let (albums, page_count) = self.parse_albums().await?;
+            if self.page_count == 0 {
+                self.page_count = page_count;
+            }
+            self.albums.insert(key.clone(), albums);
+            Ok(self.albums.get(&key))
+        }
+    }
+
+    pub async fn prev(&mut self) -> Result<Option<&Vec<Album>>> {
+        if self.page > 1 {
+            self.page -= 1;
+        } else {
+            // 当搜索器初始化后，分页总数未被初始化
+            self.page = 1;
+        }
+
+        self.get_albums().await
+    }
+
+    pub async fn next(&mut self) -> Result<Option<&Vec<Album>>> {
+        if self.page_count == 0 {
+            // 当搜索器初始化后，分页总数未被初始化
+            self.page = 1;
+        } else if self.page < self.page_count {
+            self.page += 1;
+        } else {
+            self.page_count;
+        }
+
+        self.get_albums().await
+    }
+
+    pub async fn first(&mut self) -> Result<Option<&Vec<Album>>> {
+        self.page = 1;
+        self.get_albums().await
+    }
+
+    pub async fn last(&mut self) -> Result<Option<&Vec<Album>>> {
+        if self.page_count == 0 {
+            // 解析第一页内容，并获取分页总数
+            self.next().await?;
+        }
+
+        self.page = self.page_count;
+        self.get_albums().await
+    }
+
+    pub async fn download(&mut self, idx: usize) -> Result<()> {
+        if self.page_count == 0 {
+            return Err(anyhow!("no data"));
+        }
+
+        if self.page == 0 {
+            return Err(anyhow!("no data"));
+        }
+
+        if idx == 0 {
+            return Err(anyhow!("error album index"));
+        }
+
+        let key = format!("page-{}", self.page);
+        let albums = self.albums.get(&key);
+        if albums.is_none() {
+            return Err(anyhow!("current page no data"));
+        }
+
+        let albums = albums.unwrap();
+        if idx > albums.len() {
+            return Err(anyhow!("error album index, max index: {}", albums.len()));
+        }
+
+        let index = idx - 1;
+        let album = &albums[index];
+        album.download_pictures("./").await
     }
 }
 
@@ -157,7 +258,7 @@ mod tests {
 
     #[test]
     fn test_download_album() {
-        let mut album = Album {
+        let album = Album {
             client: Arc::new(Client::new()),
             name: "壁纸".to_string(),
             url: "none".to_string()
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,181 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Write;
+use std::str::FromStr;
+
+use anyhow::anyhow;
+
+use gqwht_download::{Album, AlbumSearcher};
+
+#[derive(Debug)]
+enum Command {
+    HELP, SEARCH(String), FIRST, LAST, NEXT, PREV, DOWNLOAD(usize), QUIT, UNKNOWN, ArgumentErr(String)
+}
+
+impl FromStr for Command {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        let input = s.trim().to_uppercase();
+        let cmd = match input.as_str() {
+            "HELP" | "H" => {
+                Self::HELP
+            }
+            "FIRST" | "F" => {
+                Self::FIRST
+            }
+            "LAST" | "L" => {
+                Self::LAST
+            }
+            "NEXT" | "N" => {
+                Self::NEXT
+            }
+            "PREV" | "P" => {
+                Self::PREV
+            }
+            "QUIT" | "Q" => {
+                Self::QUIT
+            }
+            other => {
+                if other.starts_with("DOWNLOAD ") || other.starts_with("D ") {
+                    let parts = other.split_whitespace();
+                    match parts.skip(1).next() {
+                        Some(idx) => {
+                            match usize::from_str(idx) {
+                                Ok(idx) => {
+                                    Command::DOWNLOAD(idx)
+                                }
+                                Err(_) => {
+                                    Self::ArgumentErr("参数必须为数字".to_string())
+                                }
+                            }
+                        }
+                        None => {
+                            Self::ArgumentErr("缺少专辑索引参数".to_string())
+                        }
+                    }
+                } else if other.starts_with("SEARCH ") || other.starts_with("S ") {
+                    let parts = other.split_whitespace();
+                    match parts.skip(1).next() {
+                        Some(keyword) => {
+                            Self::SEARCH(keyword.to_string())
+                        }
+                        None => {
+                            Self::ArgumentErr("缺少专辑索引参数".to_string())
+                        }
+                    }
+                } else {
+                    Self::UNKNOWN
+                }
+            }
+        };
+
+        Ok(cmd)
+    }
+}
+
+fn print_albums(albums: Option<&Vec<Album>>) {
+    match albums {
+        Some(albums) => {
+            for (i, album) in albums.iter().enumerate() {
+                println!("{}: {}", i + 1, album.name);
+            }
+        }
+        None => {
+            println!("no albums");
+        }
+    }
+}
+
+async fn get_albums(searcher: &mut Option<AlbumSearcher>, command: Command) {
+    match searcher {
+        Some(ref mut searcher) => {
+            let ret = match &command {
+                Command::FIRST => searcher.first().await,
+                Command::LAST => searcher.last().await,
+                Command::PREV => searcher.prev().await,
+                Command::NEXT => searcher.next().await,
+                _ => Err(anyhow!("not support command: {:?}", &command))
+            };
+
+            match ret {
+                Ok(albums) => print_albums(albums),
+                Err(err) => println!("get albums error: {err:?}")
+            }
+        }
+        None => {
+            println!("Please search for the album first");
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let mut searcher_opt = None;
+    let mut searcher = &mut searcher_opt;
+
+    loop {
+        print!("-> ");
+        let _ = std::io::stdout().flush();
+
+        let mut line = String::new();
+        if let Err(err) = std::io::stdin().read_line(&mut line) {
+            println!("get input error: {}", err);
+        }
+
+        match FromStr::from_str(&line) {
+            Ok(cmd) => {
+                match cmd {
+                    Command::HELP => {
+                        println!("quit(q): quit tool");
+                        println!("next(n): goto next page");
+                        println!("prev(p): goto prev page");
+                        println!("first(f): goto first page");
+                        println!("last(l): goto last page");
+                        println!("download [idx](d [idx]): download album");
+                        println!("search [keyword](s [keyword]): search albums with keyword");
+                    }
+                    Command::SEARCH(keyword) => {
+                        *searcher = Some(AlbumSearcher::new(&keyword, AlbumSearcher::DEFAULT_PAGE_SIZE));
+                    }
+                    Command::FIRST => {
+                        get_albums(&mut searcher, Command::FIRST).await;
+                    }
+                    Command::LAST => {
+                        get_albums(&mut searcher, Command::LAST).await;
+                    }
+                    Command::PREV => {
+                        get_albums(&mut searcher, Command::PREV).await;
+                    }
+                    Command::NEXT => {
+                        get_albums(&mut searcher, Command::NEXT).await;
+                    }
+                    Command::DOWNLOAD(idx) => {
+                        match &mut searcher {
+                            Some(ref mut searcher) => {
+                                if let Err(err) = searcher.download(idx).await {
+                                    println!("download error: {err:?}");
+                                }
+                            }
+                            None =>{
+                                println!("Please search for the album first");
+                            }
+                        }
+                    }
+                    Command::ArgumentErr(err) => {
+                        println!("命令参数错误: {}", err);
+                    }
+                    Command::UNKNOWN => {
+                        println!("未知的命令: {}", line.trim());
+                    }
+                    Command::QUIT => {
+                        println!("bye bye.");
+                        return;
+                    }
+                }
+            }
+            Err(err) => {
+                println!("解析命令失败: {:?}", err);
+            }
+        }
+    }
+
 }
